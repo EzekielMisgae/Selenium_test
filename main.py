6160ab494c4c701786edd90b580ed8986c380371
ezekielalene <ezkielmisgae@gmail.com>
2023-02-13 00:03:56 +0300

    Comments added on the main.py file

--- main.py
+++ main.py
@@ -2,19 +2,18 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
 class TestGojoWebsite(unittest.TestCase):
 
-    def setUp(self):
-        self.driver = webdriver.Chrome() #can be elf.driver = webdriver.Chrome() only
+    def setUp(self):   #Setting up Chrome driver, navigating to website, and maximizing window for testing.
+        self.driver = webdriver.Chrome()
         self.driver.get("http://gojo.herokuapp.com")
         self.wait = WebDriverWait(self.driver, 10)
         self.driver.maximize_window()
 
-    def test_navigate_to_search(self):
+    def test_navigate_to_search(self):   #Navigates to search page after logging in and clicks search and submit buttons with wait times.
         self.test_login()
         # Wait for the search button to be visible
         search_button = self.wait.until(EC.element_to_be_clickable((By.ID, "search")))
@@ -24,7 +23,7 @@ class TestGojoWebsite(unittest.TestCase):
         submit_button.click()
         time.sleep(3)
 
-    def test_navigate_to_home(self):
+    def test_navigate_to_home(self):  #Navigates to home page after logging in.
         self.test_login()
         search_button = self.wait.until(EC.element_to_be_clickable((By.ID, "profile")))
         search_button.click()
@@ -33,13 +32,13 @@ class TestGojoWebsite(unittest.TestCase):
         home_button.click()
         time.sleep(3)
 
-    def test_sign_out(self):
+    def test_sign_out(self): #Test sign-out functionality by logging in, waiting 2 secs, and clicking logout button.
         self.test_login()
         time.sleep(2)
         sign_out_button = self.wait.until(EC.element_to_be_clickable((By.ID, "logout")))
         sign_out_button.click()
 
-    def test_post_homes(self):
+    def test_post_homes(self):  #Code waits for "post_homes" button to be clickable and then clicks it, with waits in between.
         time.sleep(5)
         post_homes_button = self.wait.until(EC.element_to_be_clickable((By.ID, "post_homes")))
         post_homes_button.click()
@@ -58,17 +57,17 @@ class TestGojoWebsite(unittest.TestCase):
         click_sign_in = self.wait.until(EC.element_to_be_clickable((By.ID, "sign-in")))
         click_sign_in.click()
 
-    def tearDown(self):
+    def tearDown(self): #Closes each test/func after completion.
         self.driver.close()
 
-def test_suite():
+def test_suite(): #Creating a test suite for TestGojoWebsite by adding 4 test cases
     suite = unittest.TestSuite()
     suite.addTest(TestGojoWebsite("test_navigate_to_search"))
     suite.addTest(TestGojoWebsite("test_navigate_to_home"))
     suite.addTest(TestGojoWebsite("test_sign_out"))
-    suite.addTest(TestGojoWebsite("test_post_homes"))
+    suite.addTest(TestGojoWebsite("test_post_homes")) #Fails since there is no element by ID called "post_homes" in the first page.
     return suite
 
-if __name__ == "__main__":
+if __name__ == "__main__": # Runs test suite with verbosity 2 via unittest TextTestRunner if module is executed as main program.
     runner = unittest.TextTestRunner(verbosity=2)
     runner.run(test_suite())
